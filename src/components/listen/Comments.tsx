d485cfe4791204869199c56124fa16fa64173ce8
Daniel Dides <senorequeso@gmail.com>
2019-09-11 23:11:38 -0500

    [listen] Form spans correct width
    Comment order is also reversed putting newest at the top.

--- src/components/listen/Comments.tsx
+++ src/components/listen/Comments.tsx
@@ -19,13 +19,13 @@ type FormElem = React.FormEvent<HTMLFormElement>;
 
 // TODO: AJAX
 const dummyComments = [
-  {time: 1234, user: "Daniel", comment: "Wow so awesome!"},
-  {time: 1555, user: "Evan", comment: "Super cool"},
-  {time: 2034, user: "Michael", comment: "Socking dongers boys"}
-]; 
+  { time: 1234, user: "Daniel", comment: "Wow so awesome!" },
+  { time: 1555, user: "Evan", comment: "Super cool" },
+  { time: 2034, user: "Michael", comment: "Socking dongers boys" }
+];
 
 const Comments = (props: CommentsProps) => {
-  
+
   const [comments, setComments] = useState<IComment[]>(dummyComments);
   const [body, setBody] = useState<string>('');
 
@@ -34,7 +34,7 @@ const Comments = (props: CommentsProps) => {
     addComment(body);
     setBody('');
   }
-  
+
   const addComment = (commentBody: string): void => {
     const newComments: IComment[] = [...comments, { time: Date.now(), user: "user", comment: commentBody }];
     setComments(newComments);
@@ -42,20 +42,18 @@ const Comments = (props: CommentsProps) => {
 
   return (
     <ListGroup className={styles.listGroup}>
-      <ListGroupItem className="list-group-item d-flex justify-content-between align-items-center">
         <Form onSubmit={handleSubmit}>
           <FormGroup>
             <Input type="text" name="comment-body" placeholder="Share your thoughts" onChange={e => setBody(e.target.value)} required value={body} />
             <Input type="submit" hidden />
           </FormGroup>
         </Form>
-      </ListGroupItem>
 
-      {comments.map((comment: IComment, index: number) => {
+      {comments.slice().reverse().map((comment: IComment, index: number) => {
         return <ListGroupItem key={index} className="list-group-item d-flex justify-content-between align-items-center">
           <span><small className="text-muted">{comment.time}</small>  <strong>{comment.user}</strong></span>
-            {comment.comment}
-          </ListGroupItem>
+          {comment.comment}
+        </ListGroupItem>
       })}
 
     </ListGroup>
