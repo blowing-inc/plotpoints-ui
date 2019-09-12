3e78a51650947a1e2dcb91c2d61d80863e9fcad2
Daniel Dides <senorequeso@gmail.com>
2019-09-11 22:50:56 -0500

    [listen] Comments are loaded from state
    Can add new comment (admittedly ugly-y) to state

--- src/components/listen/Comments.tsx
+++ src/components/listen/Comments.tsx
@@ -1,38 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { ListGroup, ListGroupItem } from 'reactstrap';
+import { ListGroup, ListGroupItem, Form, FormGroup, Input } from 'reactstrap';
 
 import styles from './Listen.module.css';
 
 
+type CommentsProps = {
+  groupId: string
+}
+
 interface IComment {
-  time: number, // seconds
-  user: string,
+  time: number
+  user: string
   comment: string
 }
 
-type CommentsProps = {
-  comments: [IComment]
-}
+type FormElem = React.FormEvent<HTMLFormElement>;
+
+// TODO: AJAX
+const dummyComments = [
+  {time: 1234, user: "Daniel", comment: "Wow so awesome!"},
+  {time: 1555, user: "Evan", comment: "Super cool"},
+  {time: 2034, user: "Michael", comment: "Socking dongers boys"}
+]; 
 
 const Comments = (props: CommentsProps) => {
+  
+  const [comments, setComments] = useState<IComment[]>(dummyComments);
+  const [body, setBody] = useState<string>('');
+
+  const handleSubmit = (e: FormElem): void => {
+    e.preventDefault();
+    addComment(body);
+    setBody('');
+  }
+  
+  const addComment = (commentBody: string): void => {
+    const newComments: IComment[] = [...comments, { time: Date.now(), user: "user", comment: commentBody }];
+    setComments(newComments);
+  }
+
   return (
     <ListGroup className={styles.listGroup}>
       <ListGroupItem className="list-group-item d-flex justify-content-between align-items-center">
+        <Form onSubmit={handleSubmit}>
+          <FormGroup>
+            <Input type="text" name="comment-body" placeholder="Share your thoughts" onChange={e => setBody(e.target.value)} required value={body} />
+            <Input type="submit" hidden />
+          </FormGroup>
+        </Form>
+      </ListGroupItem>
 
-      </ListGroupItem>
-      <ListGroupItem className="list-group-item d-flex justify-content-between align-items-center">
-        <span><small className="text-muted">21:45</small>  <strong>Daniel</strong></span>
-        😲😲😲😲
-      </ListGroupItem>
-      <ListGroupItem className="list-group-item d-flex justify-content-between align-items-center">
-        <span><small className="text-muted">19:21</small>  <strong>Evan</strong></span>
-        OMG
-      </ListGroupItem>
-      <ListGroupItem className="list-group-item d-flex justify-content-between align-items-center">
-        <span><small className="text-muted">01:23</small>  <strong>Mike</strong></span>
-        Miller is the c00lest
-      </ListGroupItem>
+      {comments.map((comment: IComment, index: number) => {
+        return <ListGroupItem key={index} className="list-group-item d-flex justify-content-between align-items-center">
+          <span><small className="text-muted">{comment.time}</small>  <strong>{comment.user}</strong></span>
+            {comment.comment}
+          </ListGroupItem>
+      })}
 
     </ListGroup>
   );
--- src/components/router/routes/Listen.tsx
+++ src/components/router/routes/Listen.tsx
@@ -1,19 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import AudioCard from 'audiocard'
 import ListenLayout from '../../layout/ListenLayout';
 
 import Comments from '../../listen/Comments';
 
-// TODO: AJAX
-const dummyComments = [
-  {"time": 1234, "user": "Daniel", "comment": "Wow so awesome!"},
-  {"time": 1555, "user": "Evan", "comment": "Super cool"},
-  {"time": 2034, "user": "Michael", "comment": "Socking dongers boys"}
-];
-
 const Listen: React.FC = () => {
-
-  const [comments, setComments] = useState([]);
 
   return (
     <ListenLayout>
@@ -23,7 +14,7 @@ const Listen: React.FC = () => {
         title="Tet"
         skipBackSeconds={30}
         skipForwardSeconds={30} />
-      <Comments comments={dummyComments} />
+      <Comments groupId="TODO"/>
     </ListenLayout>
   );
 }
