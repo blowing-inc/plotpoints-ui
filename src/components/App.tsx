8ad79d173f6e6af9d4d2feebefb3282f4c944599
Daniel Dides <senorequeso@gmail.com>
2019-09-25 19:56:20 -0500

    [app] Create AppContext

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from './layout/header//Header';
 import Router from './router/Router';
 
+const AppContext = React.createContext({});
+
+interface IAppContext {
+  auth: string,
+  setAuth: React.Dispatch<React.SetStateAction<string>>
+}
+
+const initialState: IAppContext = {
+  auth: "",
+  setAuth: (): void => {}
+}
+
+const AppContextComponent: React.FC = (props) => {
+  const [ auth, setAuth ] = useState<string>("");
+  return <AppContext.Provider value={initialState}>{props.children}</AppContext.Provider>
+}
+
 const App: React.FC = () => {
   return (
-    <div>
+    <AppContextComponent>
       <Header />
       <Router />
-    </div>
+    </AppContextComponent>
  );
 }
 
